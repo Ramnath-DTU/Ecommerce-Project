45018e0dc6faf8c3d403521d7733283dba5c6e8b
Ramnath <ramnath2544@gmail.com>
2025-08-25 22:40:56 +0530

    Fix the bug

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -5,7 +5,6 @@ import { HomePage } from "./pages/home/HomePage";
 import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/orders/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
-import {NotFoundPage} from "./pages/NotFoundPage";
 import "./App.css";
 
 function App() {
@@ -26,7 +25,6 @@ function App() {
         <Route path="checkout" element={<CheckoutPage cart={cart} />} />
         <Route path="orders" element={<OrdersPage cart={cart} />} />
         <Route path="tracking" element={<TrackingPage />} />
-        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
