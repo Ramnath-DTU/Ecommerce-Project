fd1f485da67a55de150f335d65001522d0243bb9
Ramnath <ramnath2544@gmail.com>
2025-08-25 22:22:41 +0530

    Add NotFoundPage

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -5,6 +5,7 @@ import { HomePage } from "./pages/home/HomePage";
 import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/orders/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
+import {NotFoundPage} from "./pages/NotFoundPage";
 import "./App.css";
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
         <Route path="checkout" element={<CheckoutPage cart={cart} />} />
         <Route path="orders" element={<OrdersPage cart={cart} />} />
         <Route path="tracking" element={<TrackingPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
