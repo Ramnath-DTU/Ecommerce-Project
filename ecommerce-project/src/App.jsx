6bc45c3c3c8c138f96f052439379470d0f8d3b8f
Ramnath <ramnath2544@gmail.com>
2025-09-03 13:15:59 +0530

    Remove the unnecessary images

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -6,7 +6,7 @@ import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/orders/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
-import "./App.css";
+
 
 function App() {
   const [cart, setCart] = useState([]);
